7d80351ccfe32d700b17487e48ed61a38f8a9262
Dan Burke <dburke@addictmud.org>
2021-10-24 23:15:55 -0400

    Cleanup test dir

--- ldb_test.go
+++ ldb_test.go
@@ -1,6 +1,7 @@
 package ldb_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/dwburke/ldb"
@@ -9,13 +10,14 @@ import (
 func TestOpen(t *testing.T) {
 
 	ldb, err := ldb.NewLdb(&ldb.Config{
-		Datadir:         "test",
+		Datadir:         "test_state",
 		ErrorOnNotFound: false,
 	})
 	if err != nil {
 		t.Error(err)
 	}
 	defer ldb.Close()
+	defer os.RemoveAll("test_state")
 
 	//if result != sc.Expected {
 	//t.Errorf("result is not as expected: wanted [%s], got [%s]", sc.Expected, result)
